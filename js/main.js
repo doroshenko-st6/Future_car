f3c03673b8d0bcd6d10cd1541fdd0af28b0dc378
Doroshenko_st6 <lightestwaymusic@gmail.com>
2021-03-05 20:21:37 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// SLICK SLIDER !!!!!!!!!!!!!!!!!!
+$(document).ready(function(){
+    $('.main-content-myslider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      dots: true,
+      speed: 450,
+      fade: true,
+      cssEase: 'linear',
+      arrows: false,
+      autoplaySpeed: 4000,
+    });
+  });
